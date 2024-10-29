1a0452f3470df2873359b82a073dffc147004e20
weizhonzhen <czdswzz@126.com>
2024-10-29 10:23:07 +0800

    Add files via upload

---
+++ Model/EFContext.cs
@@ -0,0 +1,90 @@
+﻿using EFCore.Extension.Aop;
+using EFCore.Extension.Base;
+using EFCore.Extension.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+
+namespace EFCore.Extension.Context
+{
+    public static class EFContext
+    {
+        public static Query QuerySql(this DbContext context, string sql, List<DbParameter> param, string key = null)
+        {
+            var query = new Query();
+            query.Data.sql = sql;
+            query.Data.param = param;
+            query.Data.key = key;
+
+            return query;
+        }
+    }
+
+    public class DataContext : IDisposable
+    {
+        private DbConnection conn;
+        internal DbCommand cmd;
+        internal ConfigData config;
+        internal bool isDispose;
+
+        public DataContext(string key)
+        {
+            try
+            {
+                DbProviderFactory dbProviderFactory;
+
+                if (key == null)
+                {
+                    dbProviderFactory = EFCoreCache.DbFactory.FirstOrDefault().Value;
+                    config = EFCoreCache.Config.FirstOrDefault().Value;
+                }
+                else
+                {
+                    EFCoreCache.DbFactory.TryGetValue(key, out dbProviderFactory);
+                    EFCoreCache.Config.TryGetValue(key, out config);
+                }
+                conn = dbProviderFactory.CreateConnection();
+                conn.ConnectionString = config.ConnStr;
+                conn.Open();
+                cmd = conn.CreateCommand();
+            }
+            catch (Exception ex)
+            {
+                BaseAop.AopException(key, string.Empty, null, ex);
+
+                if (ServiceContext.Engine.Resolve<IEFCoreAop>() == null)
+                    throw new Exception(ex.Message);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(cmd);
+            conn.Close();
+            cmd.Dispose();
+            conn.Dispose();
+            isDispose = true;
+            GC.SuppressFinalize(this);
+        }
+
+        internal void Dispose(DbCommand cmd)
+        {
+            if (cmd == null) return;
+            if (cmd.Parameters != null && config.DbType == DbTypeEnum.Oracle)
+            {
+                foreach (var param in cmd.Parameters)
+                {
+                    param.GetType().GetMethods().ToList().ForEach(m =>
+                    {
+                        if (m.Name == "Dispose")
+                            BaseEmit.Invoke(cmd, m, null);
+                    });
+                }
+            }
+            cmd.Parameters.Clear();
+        }
+    }
+}
---
+++ Model/IUnitOfWorK.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace EFCore.Extension.Context
+{
+    internal interface IUnitOfWorK
+    {
+        DataContext Contexts(string key);
+
+        DataContext Context { get; set; }
+    }
+
+
+    internal class UnitOfWorK : IUnitOfWorK, IDisposable
+    {
+        private DataContext _Context;
+
+        public DataContext Context
+        {
+            set { _Context = value; }
+            get
+            {
+                if (_Context != null && !_Context.isDispose)
+                    return _Context;
+                else
+                {
+                    _Context = new DataContext(EFCoreCache.Config.FirstOrDefault().Value?.Key);
+                    return _Context;
+                }
+            }
+        }
+
+        private readonly ConcurrentDictionary<string, DataContext> list = new ConcurrentDictionary<string, DataContext>();
+
+        public void Dispose()
+        {
+            foreach (var item in list)
+            {
+                if (!item.Value.isDispose)
+                    _Context?.Dispose();
+                item.Value?.Dispose();
+            }
+            list.Clear();
+        }
+
+        public DataContext Contexts(string key)
+        {
+            if (!EFCoreCache.Config.Keys.ToList().Exists(a => a == key) && EFCoreCache.Config.Count > 1)
+                throw new Exception($"不存在数据库Key:{key}");
+
+            if (string.IsNullOrEmpty(key))
+                key = EFCoreCache.Config.FirstOrDefault().Value?.Key;
+
+            DataContext data;
+            list.TryGetValue(key, out data);
+            if (data == null || data?.isDispose == true)
+            {
+                list.TryRemove(key, out data);
+                data = new DataContext(key);
+                list.TryAdd(key, data);
+            }
+            return data;
+        }
+    }
+}
